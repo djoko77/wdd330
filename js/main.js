b99efe444fead75c03599f2c1ab9a7b5a481f841
Rombie Lagunzad <41833766+djoko77@users.noreply.github.com>
2022-01-04 00:03:26 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -1,30 +1,49 @@
+/************************************/
+/**** Dynamic Table of Contents ****/
+/**********************************/
+
 function createList() {
 
-const links = [
-    {
-      label: "Week 1 Notes",
-      url: "week1/index.html"
-    }
-  ];
+    // Weekly list of links
+    const links = [
+        {
+            label: "Week 1 Notes",
+            url: "week1/index.html"
+        },
+        {
+            label: "Week 2 Notes",
+            url: "week2/index.html"
+        }
+    ];
 
-links.forEach(weekly => {
+        //  For each of the items in the array of links
+        links.forEach(weekly => {
+        
+        // Create an li element
+        let li = document.createElement("li");
 
-let li = document.createElement("li");
+        // Create an anchor element
+        let a = document.createElement("a");
 
-let a = document.createElement("a");
+        // Create the text node 
+        let textNode = document.createTextNode(weekly.label);
+        
+        // Append the text node to anchor element
+        a.appendChild(textNode);
 
-let textNode = document.createTextNode(weekly.label);
+        // Set the href attribute
+        a.setAttribute("href", weekly.url);
+    
+        // Append the anchor element to li element
+        li.appendChild(a);
+        
+        // Select the ol element from index.html
+        let list = document.getElementById("activity");
+        
+        // Append the li element to the list
+        list.appendChild(li);
+        }
+        )}
 
-a.appendChild(textNode);
-
-a.setAttribute("href", weekly.url);
-
-li.appendChild(a);
-
-let list = document.getElementById("activity");
-
-list.appendChild(li);
-}
-)}
-
+// Run the function
 createList();
