77b2c2347eeb649ae3504880de47c79d38dac397
Rombie Lagunzad <41833766+djoko77@users.noreply.github.com>
2022-01-13 02:23:37 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -20,28 +20,28 @@ function createList() {
         links.forEach(weekly => {
         
         // Create an li element
-        let li = document.createElement("li");
+        let li = document.createElement("li")
 
         // Create an anchor element
-        let a = document.createElement("a");
+        let a = document.createElement("a")
 
         // Create the text node 
-        let textNode = document.createTextNode(weekly.label);
+        let textNode = document.createTextNode(weekly.label)
         
         // Append the text node to anchor element
-        a.appendChild(textNode);
+        a.appendChild(textNode)
 
         // Set the href attribute
-        a.setAttribute("href", weekly.url);
+        a.setAttribute("href", weekly.url)
     
         // Append the anchor element to li element
-        li.appendChild(a);
+        li.appendChild(a)
         
         // Select the ol element from index.html
-        let list = document.getElementById("activity");
+        let list = document.getElementById("activity")
         
         // Append the li element to the list
-        list.appendChild(li);
+        list.appendChild(li)
         }
         )}
 
