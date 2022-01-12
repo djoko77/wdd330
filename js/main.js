598a9abbde476baa3395b7e8d2ef880d85f8dcaa
Rombie Lagunzad <41833766+djoko77@users.noreply.github.com>
2022-01-13 01:39:46 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -130,7 +130,7 @@ function conAddition() {
     let numbers = parseInt(document.getElementById("number").value)
     let sum = 0;
 
-    for (let i = 0; i < number; i++) {
+    for (let i = 0; i <= number; i++) {
         sum += i;
     }
 
