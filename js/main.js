7fa220cc4141252c99d658a44def7c7b65ad5ca7
Rombie Lagunzad <41833766+djoko77@users.noreply.github.com>
2022-01-13 03:09:41 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -128,16 +128,13 @@ and output the result to the div.
 function conAddition(numbers) {
 
     if (Number.isInteger(numbers)) {
-
         let numbers = parseInt(numbers)
         let sum = 0
-        
         for (let i = 0; i <= numbers; i++) {
             sum += i;
         }
     document.getElementById("addition-box").innerHTML = sum
     }
-
     else {
         document.getElementById("addition-box").innerHTML = "Wrong input! Please enter a number"
     }
