641927adcd2ef65a939f31bf7db73c19d47efce1
Rombie Lagunzad <41833766+djoko77@users.noreply.github.com>
2022-02-19 23:16:29 +0800

    fix the comment section

--- js/comment.js
+++ js/comment.js
@@ -1,113 +1,102 @@
-// class Comment {
-//     constructor (type) {
-//         this.type = type;
-        
-//     }
-// }
+class CommentModel {
+  constructor(type) {
+      this.type = type
+  }
+}
+
+class Comment {
+  constructor(name, date, content, type) {
+      this.name = name,
+      this.date = date,
+      this.content = content
+      this.type = new CommentModel(type)
+  }
+}
+
+function getAllComments() {
+      const allComments = readFromLS();
+      allComments.forEach(element => {
+      const commentList = renderCommentList(element)
+      showCommentList(commentList);
+  })
+}
+
+function renderCommentList(allComments) {
+      let dateConvert = allComments.date;
+      const today = new Date(dateConvert);
+      const commentList = document.createElement("li");
+      commentList.classList.add('single-comment');
+      commentList.innerHTML = `<div>
+      <h4>${allComments.name}</h4>
+      <p>${allComments.content} - ${today.toDateString()}</p></div>`;
+      return commentList;
+  }
+  
+function showCommentList(commentList) {
+      document.querySelector('#full-comments').appendChild(commentList);
+  }
+      
+function showCommentListFiltered(commentList) {
+
+document.querySelector('#filtered-comments').appendChild(commentList);
+
+}
+
+function addComment() {
+  var comments = []
+  let commentText = document.getElementById("input-comment").value;
+  
+  let hikeName = document.getElementById("add-button").attributes["name"].value;
+  //console.log(hikeName);
+  let newComment = new Comment(hikeName, Date.now(), commentText, 'hike')
+
+  comments.push(newComment);
+  writeToLS(newComment);
+  //console.log(JSON.stringify(newComment));
+  
+  document.querySelector('#filtered-comments').innerHTML = "";
+  filterCommentsByName(hikeName);
+
+}
+
+function filterCommentsByName(hikeName) {
+
+  let comments = readFromLS();
+
+  var filteredComments = comments.filter(comment => comment.name == hikeName)
+
+  filteredComments.forEach(element => {
+      const commentList = renderCommentList(element)
+      showCommentListFiltered(commentList);
+})
+//console.log(JSON.stringify(filteredComments))
+//return filteredComments;
+}
 
 function writeToLS(newComment) {
-    const allComments = readFromLS();
-    allComments.push(newComment);
-    //console.log(JSON.stringify(allComments));
-    localStorage.setItem('allComments', JSON.stringify(allComments));
+  const allComments = readFromLS();
+  allComments.push(newComment);
+  //console.log(JSON.stringify(allComments));
+  localStorage.setItem('allComments', JSON.stringify(allComments));
 }
 
 function readFromLS() {
-    const allCommentsText = localStorage.getItem('allComments');
-    let allComments = [];
+  const allCommentsText = localStorage.getItem('allComments');
+  let allComments = [];
 
-    if(allCommentsText) {
-        allComments = JSON.parse(allCommentsText);
-    }
-    else if (allComments == undefined || allComments == null){
-        localStorage.clear();
-        allComments = JSON.parse(allCommentsText);
-    }
-    return allComments;
+  if(allCommentsText) {
+      allComments = JSON.parse(allCommentsText);
+  }
+  else if (allComments == undefined || allComments == null){
+      localStorage.clear();
+      allComments = JSON.parse(allCommentsText);
+  }
+  return allComments;
 }
 
-    // getAllComments 
-  function getAllComments() {
-         const allComments = readFromLS();
-         allComments.forEach(element => {
-          const commentList = renderCommentList(element)
-          showCommentList(commentList);
-        //console.log(JSON.stringify(commentsList));
-        // return commentsList;
-})
-  }
-  
-    //  renderCommentList for page 1
-    function renderCommentList(allComments) {
-        // console.log(JSON.stringify(allComments));
 
-        let dateConvert = allComments.date;
-        const today = new Date(dateConvert);
-        const commentList = document.createElement("li");
-        commentList.classList.add('single-comment');
-        commentList.innerHTML = `
-        <h4>${allComments.name}</h4>
-        <br>
-        <p>${allComments.content} - ${today.toDateString()}</p>
-        `;
-        
-         return commentList;
-    }
- 
-    function showCommentList(commentList) {
-      document.querySelector('#full-comments').appendChild(commentList);
-    }
-
-    // function showCommentListFiltered(commentList) {
-    //   document.querySelector('#filter-comments').appendChild(commentList);
-    // }
-
-  
-    // Filter comments for subcontents
-    function filterCommentsByName(hikeName) {
-      //document.getElementById("filter-comments").innerHTML = '';
-      let filteredComments = [];
-  
-      const allComments = readFromLS();
-  
-      if (hikeName == 'Bechler Falls') {
-        filteredComments = hikeOne(allComments);
-        alert(JSON.stringify(filteredComments))
-      }
-  
-      else if (hikeName == 'Teton Canyon') {
-        filteredComments = hikeTwo(allComments);
-      }
-  
-      else if (hikeName == 'Denanda Falls') {
-        filteredComments = hikeThree(allComments);
-      }
-  
-      filteredComments.forEach(comment => {
-        const commentList = renderCommentList(comment)
-        //showCommentListFiltered(commentList);
-        //alert(JSON.stringify(commentList))
-    })
-    return filteredComments;
-    }
-  
-    function hikeOne(comments) {
-      return comments.filter(comment => {
-          return comment.name == 'Bechler Falls';
-      })
-    }
-  
-    function hikeTwo(comments) {
-      return comments.filter(comment => {
-          return comment.name == 'Teton Canyon';
-      })
-    }
-  
-    function hikeThree(comments) {
-      return comments.filter(comment => {
-          return comment.name == 'Denanda Falls';
-      })
-    }
+export default { addComment, getAllComments, filterCommentsByName }
 
 
-export default { readFromLS, writeToLS, getAllComments, filterCommentsByName }
+
+
--- js/week7.js
+++ js/week7.js
@@ -19,20 +19,3 @@ textButton.addEventListener('click', () => {
     .catch( error => console.log('There was an error:', error))
 },false);
 
-// const textButton = document.getElementById('number');
-// const apiButton = document.getElementById('chuck');
-// const outputDiv = document.getElementById('output');
-
-// const textURL = 'http://numbersapi.com/random';
-
-// textButton.addEventListener('click', () => {    
-//     fetch(textURL)    
-//     .then( response => {        
-//         outputDiv.innerHTML = 'Waiting for response...';   
-//      if(response.ok) {        
-//          return response;    }        
-//          throw Error(response.statusText);    })    
-//          .then( response => response.text() )    
-//          .then( text => outputDiv.innerText = text )    
-//          .catch( error => console.log('There was an error:', error))}
-//          ,false);
