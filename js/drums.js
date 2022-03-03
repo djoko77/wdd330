2b4fc21d8578696c13f6a291781bd2f756e2aa7a
Rombie Lagunzad <41833766+djoko77@users.noreply.github.com>
2022-03-03 19:52:01 +0800

    team activity

--- js/drums.js
+++ js/drums.js
@@ -1,10 +1,25 @@
+let count = 0;
 
-const buttons = document.querySelectorAll('.key');
-buttons.forEach(button => button.addEventListener('click', event => {
-    const dataKey = event.currentTarget.dataset.key;
-    console.log(event.currentTarget.dataset.key);
-    selectedAudio = "audio[data-key=\""+dataKey+"\"]"
-    console.dir(selectedAudio)
-    const audio = document.body.querySelector(selectedAudio);
+window.addEventListener('keydown', function(e) {
+    let code = `audio[data-key="${e.keyCode}"]`;
+
+    const audio = document.querySelector(code);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    audio.currentTime = 0;
     audio.play();
-}))
+    key.classList.add('playing');
+
+    count += 10;
+    key.style.marginTop = count + "px";
+
+    if (count == 100) {
+        count = 0;
+        key.style.marginTop = count + "px";
+    }
+
+    setTimeout(() => {
+        key.classList.remove("playing");
+    }, 1000);
+
+})
+
