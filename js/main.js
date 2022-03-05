7d23b7f00ba415e471a0cbb6aa61b01d972d408f
Rombie Lagunzad <41833766+djoko77@users.noreply.github.com>
2022-03-06 00:27:32 +0800

    add week 10 notes

--- js/main.js
+++ js/main.js
@@ -38,6 +38,10 @@ function createList() {
         {
             label: "Week 9 Notes",
             url: "week9/index.html"
+        },
+        {
+            label: "Week 10 Notes",
+            url: "week10/index.html"
         }
     ];
 
