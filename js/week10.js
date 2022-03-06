666c61855dae492f76220798dbc607e84d8187d3
Rombie Lagunzad <41833766+djoko77@users.noreply.github.com>
2022-03-06 20:44:16 +0800

    fix the issue

--- js/week10.js
+++ js/week10.js
@@ -1,43 +1,45 @@
-var regButton = document.querySelectorAll('#reg-button')[0];
+// var regButton = document.querySelectorAll('#reg-button')[0];
 
-regButton.addEventListener('click', (event) => {
-    window.location = "https://djoko77.github.io/wdd330/week10/thanks-reg.html"
-})
+// regButton.addEventListener('click', (event) => {
+//     window.location = "https://djoko77.github.io/wdd330/week10/thanks-reg.html"
+// })
 
-var subsButton = document.querySelectorAll('#subs-button')[0];
+var form2 = document.querySelectorAll('.form2')[0];
 var email = document.getElementById('user-email');
-var username = document.getElementById('user-name');
+const error = document.querySelector('#user-email + div.error');
 
 email.addEventListener("input", (event) => {
     
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity("Please enter a valid e-mail address!");
-        email.reportValidity();
-    } 
-
-    else if (email.validity.valueMissing) {
-        email.setCustomValidity("Email address cannot be blank");
-        email.reportValidity();
-    } 
-
-    else {
-    email.setCustomValidity("");
-}
+    if (email.validity.valid) {
+        error.textContent = ''; // Reset the content of the message
+        error.className = 'error'; // Reset the visual state of the message
+      } else {
+        // If there is still an error, show the correct error
+        showError();
+      }
 })
 
-username.addEventListener("input", (event) => {
-    
-    if (username.validity.valueMissing) {
-        username.setCustomValidity("Us cannot be blank");
-        username.reportValidity();
-    } 
-    else {
-        username.setCustomValidity("");
+form2.addEventListener('submit', (event) => {
+    if(!email.validity.valid) {
+        // If it isn't, we display an appropriate error message
+        showError();
+        // Then we prevent the form from being sent by canceling the event
+        event.preventDefault();
+      }
+    });
+
+function showError() {
+    if(email.validity.valueMissing) {
+        // If the field is empty,
+        // display the following error message.
+        error.textContent = 'Blank field: Please enter your e-mail address!';
+      } else if(email.validity.typeMismatch) {
+        // If the field doesn't contain an email address,
+        // display the following error message.
+        error.textContent = 'Please enter a valid e-mail address!';
+      }
+      error.className = 'error active';
 }
-})
 
 
-subsButton.addEventListener('click', (event) => {
-    window.location = "https://djoko77.github.io/wdd330/week10/thanks-subs.html"
-});
 
